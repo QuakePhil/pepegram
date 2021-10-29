d0acfb170f3ac208e97c5bdf10f6f202ed3bdf0c
Phil <phil.nyc@gmail.com>
2021-10-29 15:31:47 -0400

    Update lambda.py
    error handling

--- lambda.py
+++ lambda.py
@@ -6,15 +6,25 @@ def sanitize(email):
     # return re.sub('[^@]+@[^@]+\.[^@]+', '', email)
 
 def subscribe(event):
+    if 'queryStringParameters' not in event:
+        return {
+            'statusCode': 500,
+            'body': 'Missing: event[queryStringParameters]'
+        }
+    if 'email' not in event['queryStringParameters']:
+        return {
+            'statusCode': 500,
+            'body': 'Missing: event[queryStringParameters][email]'
+        }
+
     email = sanitize(event['queryStringParameters']['email'])
     encoded_string = email.encode("utf-8")
 
     boto3.resource("s3").Bucket("pepegram").put_object(Key="subscribe/"+email, Body=encoded_string)
-    return email
-
-def lambda_handler(event, context):
-    # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps(subscribe(event))
+        'body': json.dumps(email)
     }
+
+def lambda_handler(event, context):
+    return subscribe(event)
