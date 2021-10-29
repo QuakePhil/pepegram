11f065b2ec26d21c20bef66b43b62c98ffbc845e
Phil <phil.nyc@gmail.com>
2021-10-29 19:43:14 -0400

    Update blast.py

--- lambda/blast.py
+++ lambda/blast.py
@@ -1,18 +1,16 @@
 # pepegram-blast/lambda_function.py
 import boto3, re, json
 
-def to():
-    to = ''
-    comma = ''
+def recipients():
+    result = []
     bucket = boto3.resource("s3").Bucket("pepegram")
     for path in bucket.objects.filter(Prefix='subscribe/'):
-        to += comma + path.key[10:]
-        comma = ','
-    # bucket.objects.filter(Prefix="subscribe/").delete()
-    return to
+        result.append(path.key[10:])
+    return result
 
-def blast(event):
-    recipients = to()
+
+def blast_one_email(recipient):
+    # bucket.objects.filter(Prefix="subscribe/"+recipient).delete()
     BODY_HTML = """<html>
     <head></head>
     <body>
@@ -30,34 +28,30 @@ def blast(event):
                 )
     # boto3.client('ses').verify_email_identity(EmailAddress="phil.nyc@gmail.com")
 
-    response = boto3.client('ses').send_email(
-        Destination={
-            'ToAddresses': [
-                recipients,
-            ],
-        },
-        Message={
-            'Body': {
-                'Html': { 'Charset': "UTF-8", 'Data': BODY_HTML, },
-                'Text': { 'Charset': "UTF-8", 'Data': BODY_TEXT, },
+    try:
+        response = boto3.client('ses').send_email(
+            Source="phil.nyc@gmail.com",
+            Destination={'ToAddresses': [ recipient ]},
+            Message={
+                'Body': {   'Html': { 'Charset': "UTF-8", 'Data': BODY_HTML, },
+                            'Text': { 'Charset': "UTF-8", 'Data': BODY_TEXT, },},
+                'Subject': {'Charset': "UTF-8",'Data': 'Pepegram subscription',},
             },
-            'Subject': {
-                'Charset': "UTF-8",
-                'Data': 'Pepegram subscription',
-            },
-        },
-        Source="phil.nyc@gmail.com",
-    )
-    # except ClientError as e:
-    #     print(e.response['Error']['Message'])
-    # else:
-    print(response) # ['MessageId'])
-    print(response['Error'])
-    
+        )
+        print("Email sent to: " + recipient)
+        print(response) # ['MessageId'])
+    except Exception as e:
+        print("Error while emailing " + recipient)
+        print(e)
+
+def blast():
+    emails = recipients()
+    for email in emails:
+        blast_one_email(email)
     return {
         'statusCode': 200,
-        'body': json.dumps(recipients)
+        'body': json.dumps(emails)
     }
 
 def lambda_handler(event, context):
-    return blast(event)
+    return blast()
